a64f46bc8dcd3541b18c38c49e70b875305208ff
Mosse-Sox <autumngreenie990@gmail.com>
2023-06-15 05:20:03 +0000

    made boxes functional, they add numbers to an array

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
+let dataSets = [];
+let numberOfInputBoxes = 3;
+
 $(document).ready(function() {
-  $("#submit-button").click(function() {
+  $("#generate-button").click(function() {
     const dataSet1 = $("#data-set-1").val();
-    alert(dataSet1);
+    const dataSet2 = $("#data-set-2").val();
+    const dataSet3 = $("#data-set-3").val();
+    dataSets.push(dataSet1);
+    dataSets.push(dataSet2);
+    dataSets.push(dataSet3);
+    alert(dataSets);
   });
+
+
 
 
 });
