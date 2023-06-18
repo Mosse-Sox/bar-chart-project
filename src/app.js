3c46918ee76b57aa2cc2193a250be5779ab2d34f
Mosse-Sox <autumngreenie990@gmail.com>
2023-06-18 21:28:30 +0000

    Added x and y axis boxes, new input box buttons are added above the x and y axis boxes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
-// import { inputBoxCount } from "../src/input-box-maker.js";
+import { inputBoxCount } from "../src/input-box-handler.js";
 import { collectData, collectName } from "../src/collectors.js";
+import { inputBoxMaker } from "../src/input-box-handler.js";
 
 $(document).ready(function () {
+
+  // what happens when you click the generate bar chart button
   $("#generate-button").click(function (event) {
     event.preventDefault();
 
@@ -10,7 +13,18 @@ $(document).ready(function () {
 
     $("#output-div").html(`<p>${data}</p>`);
     $("#output-div").append(`<p>${dataNames}</p>`);
+    $("#output-div").append(`<p>${inputBoxCount}</p>`);
   });
+
+
+  // this function executes when the "Add More Data" button gets pressed, it calls another function that creates an input box
+  $("#new-input-box-button").click(function (event) {
+    event.preventDefault();
+
+    inputBoxMaker();
+  });
+
+
 });
 
 
--- src/input-box-handler.js
+++ src/input-box-handler.js
@@ -1,25 +1,23 @@
 export var inputBoxCount = 3;
 
-$(document).ready(function() {
+function generateDataID(inputBoxCount) {
+  return "data-set-" + inputBoxCount;
+}
+
+function generateLabelID(inputBoxCount) {
+  return "data-set-" + inputBoxCount + "-label";
+}
+
+export function inputBoxMaker() {
+  inputBoxCount++;
+
+  $(`<li>
+      <input type="text" class="data-set" id="${generateDataID(inputBoxCount)}" placeholder="Dataset ${inputBoxCount}"></input>
+      <input type="text" class="data-label" id="${generateLabelID(inputBoxCount)}" placeholder="Name of Data"></input>
+    </li>`).insertBefore("#last-list-item");
+};
 
 
-  function generateDataID(inputBoxCount) {
-    return "data-set-" + inputBoxCount;
-  }
 
-  function generateLabelID(inputBoxCount) {
-    return "data-set-" + inputBoxCount + "-label";
-  }
 
-  $("#new-input-box-button").click(function(event) {
-    event.preventDefault();
-
-    inputBoxCount++;
-    $("#input-boxes").append(`<li>
-    <input type="text" class="data-set" id="${generateDataID(inputBoxCount)}" placeholder="Dataset ${inputBoxCount}"></input>
-    <input type="text" class="data-label" id="${generateLabelID(inputBoxCount)}" placeholder="Name of Data"></input>
-    </li>`);
-
-  });
-});
 
